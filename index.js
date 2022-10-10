9275ba03f12927f82b7b521c72061e1a01194e4e
MainUddin1223 <2021md.main@gmail.com>
2022-10-10 14:25:50 +0600

    commit message

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const PORT = 3006 ;
+const app =  express() ;
+
+
+
+
+
+
+
+
+app.get('/test',(req,res)=>{
+    res.json({status:true , message:"server respond successfully"})
+})
+app.listen(PORT,()=>console.log("server is running on 3006"))
